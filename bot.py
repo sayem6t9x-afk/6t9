b9cbb8096456bbb5e77287cbae66d934610eefd0
sayem6t9x-afk <sayem6t9x@gmail.com>
2026-07-30 13:36:23 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -31,8 +31,8 @@ ADMIN_ID = 5605925198
 ADMIN_USERNAME_LINK = "[@sayem6t9](https://t.me/sayem6t9)"
 BANNED_MSG = f"🚫 **You have been BANNED from using this bot!**\n\nTo request an unban, please message the Admin: {ADMIN_USERNAME_LINK}"
 
-# 🐘 SUPABASE POSTGRESQL DATABASE URL (New Updated URL)
-DATABASE_URL = "postgresql://postgres:WQsa9069%23%2A6T9@db.cvqaqgqzlgbrlntvvlfn.supabase.co:5432/postgres"
+# 🐘 SUPABASE POSTGRESQL DATABASE URL (Vercel Serverless Ready - Port 6543)
+DATABASE_URL = "postgresql://postgres.cvqaqgqzlgbrlntvvlfn:WQsa9069%23%2A6T9@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres"
 
 def get_db_connection():
     return psycopg2.connect(DATABASE_URL)
